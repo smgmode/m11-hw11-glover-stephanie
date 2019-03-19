3d71f4b601bb2fe0ddea4feafd52757bab60a4da
Stephanie Glover <stephanie.glover@ufl.edu>
2019-03-19 14:08:07 -0700

    Add functionality to JS file

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
+function loopThrough() {
+
+  const changeColor = () => this.classList.toggle('red'); //function defined
+
+  for(let i = 0; i < this.textContent.length; i++) { //for loop
+     changeColor(); //initialize function inside of the for loop
+};
+
+};
 
 
 //The Event Listener
